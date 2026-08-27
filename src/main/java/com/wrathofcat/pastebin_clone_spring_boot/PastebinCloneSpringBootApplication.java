04ab6230ba399eec82be7dfe41c27cb5a93e0376
wrath-of-cat <215492206+wrath-of-cat@users.noreply.github.com>
2026-08-27 19:33:17 +0530

    Initializing the project

---
+++ src/main/java/com/wrathofcat/pastebin_clone_spring_boot/PastebinCloneSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.wrathofcat.pastebin_clone_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinCloneSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastebinCloneSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wrathofcat/pastebin_clone_spring_boot/PastebinCloneSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrathofcat.pastebin_clone_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastebinCloneSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
